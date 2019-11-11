50c190b7798455e7f8374602d05464a58bc66b66
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-11 17:26:18 +0000

    added import

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router } from 'react';
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
