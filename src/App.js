34ea0c2ea7115fc1908706e729d998c8ce982fd4
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-11 17:31:06 +0000

    wrapped

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
+<Router>
 const App = () => (
   <div>
     <Navigation />
   </div>
 );
+
+</Router>
 
 export default App;
